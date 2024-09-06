bfa012cfea7c127a3540ea54aeb609485bb04693
OlehTurchynDev <133028550+OlehTurchynDev@users.noreply.github.com>
2024-09-06 12:18:02 +0300

    Async_Promises_Fetch

---
+++ Fetch/index.js
@@ -0,0 +1,4 @@
+// write code to get data from https://jsonplaceholder.typicode.com/users (receive statusCode and data)
+
+// write code to get data from https://jsonplaceholder.typicode.com/users then get id of first user and using this id get posts of this user using
+// https://jsonplaceholder.typicode.com/posts?userId={id of ur user}
